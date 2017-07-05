c6ad92440e6a7f2d7ec6dc5b09b12bcdfda56eb8
zakizaki <zackey828@gmail.com>
2017-07-05 19:38:08 +0900

    2!

--- app/src/main/java/com/example/murota_lab/samplegit/MainActivity.java
+++ app/src/main/java/com/example/murota_lab/samplegit/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.murota_lab.samplegit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-/**git!?3**/
+/**git!?4**/
 
 public class MainActivity extends AppCompatActivity {
 
